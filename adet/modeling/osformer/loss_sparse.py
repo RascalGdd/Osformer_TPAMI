6962af91cb13133e291b5d8b42c5bc56f4014c00
Diandian Guo <guodiandian1998@163.com>
2023-04-17 15:50:09 +0200

    debug

--- adet/modeling/osformer/loss_sparse.py
+++ adet/modeling/osformer/loss_sparse.py
@@ -163,6 +163,10 @@ class SparseInstCriterion(nn.Module):
         assert 'pred_boxes' in outputs
         idx = self._get_src_permutation_idx(indices)
         src_boxes = outputs['pred_boxes'][idx]
+
+        for t in targets:
+            t['boxes'] = masks_to_boxes(t["masks"])
+
         target_boxes = torch.cat([t['boxes'][i] for t, (_, i) in zip(targets, indices)], dim=0)
 
         loss_bbox = F.l1_loss(src_boxes, target_boxes, reduction='none')
